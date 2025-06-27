9a77c186e48f1bfe8a27eb5d0f549a5343b2947f
0xedev <oxdeve@gmail.com>
2025-06-27 13:47:11 +0100

    gt

--- script/CreatePresale.s.sol
+++ script/CreatePresale.s.sol
@@ -5,6 +5,7 @@ import {Script, console} from "forge-std/Script.sol";
 import {PresaleFactory} from "../src/contracts/PresaleFactory.sol";
 import {Presale} from "../src/contracts/Presale.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract CreatePresale is Script {
@@ -47,7 +48,7 @@ contract CreatePresale is Script {
 
         // --- Instantiate Contracts ---
         PresaleFactory factory = PresaleFactory(payable(factoryAddress));
-        IERC20 presaleToken = IERC20(presaleTokenAddress);
+        IERC20Metadata presaleToken = IERC20Metadata(presaleTokenAddress);
 
         // --- Prepare Presale Options ---
         uint256 startTime = block.timestamp + startOffset;
@@ -70,14 +71,14 @@ contract CreatePresale is Script {
 
         // Calculate required token deposit
         // For a basic presale, this is usually: hardCap * presaleRate * 10**token.decimals() / currencyDecimals
-        uint8 tokenDecimals = presaleToken.decimals();
+        uint8 decimals = presaleToken.decimals();
         uint8 currencyDecimals = 18;
         if (currencyTokenAddress != address(0)) {
-            try IERC20(currencyTokenAddress).decimals() returns (uint8 dec) {
+            try IERC20Metadata(currencyTokenAddress).decimals() returns (uint8 dec) {
                 currencyDecimals = dec;
             } catch {}
         }
-        uint256 tokensNeeded = (hardCap * presaleRate * (10 ** tokenDecimals)) / (10 ** currencyDecimals);
+        uint256 tokensNeeded = (hardCap * presaleRate * (10 ** decimals)) / (10 ** currencyDecimals);
         options.tokenDeposit = tokensNeeded;
 
         console.log("Required Presale Token Deposit:", tokensNeeded);
